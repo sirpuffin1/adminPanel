7f2f2b3093623a683d6a6f85503f6c29345d192c
sirpuffin1 <angelcruz981@yahoo.com>
2023-03-09 08:47:50 -0800

    chore(server): configure server boilerplate

--- server/index.js
+++ server/index.js
@@ -6,3 +6,14 @@ import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
 
+/* Configuration */
+dotenv.config()
+const app = express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+
