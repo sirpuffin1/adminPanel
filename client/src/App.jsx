2f25d798909b515a87fffa8ec67ab739543daaef
sirpuffin1 <angelcruz981@yahoo.com>
2023-03-09 10:08:45 -0800

    feat(app): establish theme using global state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme.js"
+import { useSelector } from "react-redux"
+import { useMemo } from "react"
 
 function App() {
+  const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
       
     </div>
   )
