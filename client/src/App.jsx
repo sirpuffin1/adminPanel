0ccc49438007d3b32c0c948a4c716edf8b0d7281
sirpuffin1 <angelcruz981@yahoo.com>
2023-03-28 11:53:31 -0700

    feat(app): create/import customers route/scene

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Dashboard from "./scenes/dashboard/index"
 import Layout from "./scenes/layout/index"
 import Products from "./scenes/products"
+import Customers from "./scenes/customers"
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
