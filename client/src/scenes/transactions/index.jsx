8ec6c0e1f68968a7b636f8ee18557b26e06509a8
sirpuffin1 <angelcruz981@yahoo.com>
2023-03-30 10:25:08 -0700

    feat(app): create/import transaction route/scene

---
+++ client/src/scenes/transactions/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid"
+import { useGetTransactionsQuery } from "../../state/api"
+import Header from "../../components/Header"
+
+const index = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default index;
