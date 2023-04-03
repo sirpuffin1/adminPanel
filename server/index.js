541f28b2e90f8364d23f2ba25149e450ea2e3328
sirpuffin1 <angelcruz981@yahoo.com>
2023-04-03 15:07:21 -0700

    feat(models): create/import overallStats

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ import User from './models/User.js';
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js"
 import Transaction from './models/Transaction.js'
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js'
+import OverallStat from './models/OverallStat.js'
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js'
 /* Configuration */
 dotenv.config()
 const app = express();
