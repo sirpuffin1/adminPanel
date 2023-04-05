b2cc644e46bab5239a2b416cd77026bca288739f
sirpuffin1 <angelcruz981@yahoo.com>
2023-04-05 12:04:50 -0700

    feat(daily): create scene/route

---
+++ client/src/scenes/daily/index.jsx
@@ -0,0 +1,17 @@
+import { useState, useMemo } from "react";
+import { Box, useTheme } from "@mui/material"
+import Header from "../../components/Header"
+import OverviewChart from "../../components/OverviewChart"
+import { useGetSalesQuery } from "../../state/api";
+import ReactDatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+
+const Daily = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Daily;
